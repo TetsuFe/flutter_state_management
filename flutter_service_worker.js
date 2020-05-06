fe9cf54b068d8db5cffa6939aa7023e144a0e7dd
TetsuFe <TetsuFe@users.noreply.github.com>
2020-05-06 00:15:12 +0000

    deploy: b215fa16b275ab1a415ab5615756b6b56591b0e1

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -1,19 +1,19 @@
 'use strict';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "favicon.png": "5dcef449791fa27946b3d35ad8803796",
-"index.html": "cf851b0227abb4790972313dd86517b5",
+  "index.html": "cf851b0227abb4790972313dd86517b5",
 "/": "cf851b0227abb4790972313dd86517b5",
-"manifest.json": "10650a18edaf253be7032f884bc3da07",
+"icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
+"icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "assets/story/1/hokuma.jpg": "da4cb011fdae64db030c5c7cb747dacd",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "115e937bb829a890521f72d2e664b632",
-"assets/AssetManifest.json": "2efbb41d7877d10aac9d091f58ccd7b9",
 "assets/FontManifest.json": "01700ba55b08a6141f33e168c4a6c22f",
-"assets/LICENSE": "68f2017c568a8777112d3159baa292e9",
 "assets/fonts/MaterialIcons-Regular.ttf": "56d3ffdef7a25659eab6a68a3fbfaf16",
+"assets/AssetManifest.json": "2efbb41d7877d10aac9d091f58ccd7b9",
+"assets/LICENSE": "68f2017c568a8777112d3159baa292e9",
+"favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "main.dart.js": "447b1cd52d8a5cac8759731f68a8b644",
-"icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1"
+"manifest.json": "10650a18edaf253be7032f884bc3da07"
 };
 
 self.addEventListener('activate', function (event) {
